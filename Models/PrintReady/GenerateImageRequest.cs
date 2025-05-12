aecbd38a428cfeda7d31f24f64c98f1022de37dc
Michael McAiney <michael.mcainey@gmail.com>
2025-05-12 01:26:52 +0100

    Create print file

--- Models/PrintReady/GenerateImageRequest.cs
+++ Models/PrintReady/GenerateImageRequest.cs
@@ -5,10 +5,12 @@ public class GenerateImageRequest
 {
     public long ProductVariantId { get; set; }
     public int ProductType { get; set; }
-    public List<GenerateImage> GenerateImages { get; set; }
+    public List<GenerateImage> GenerateImages { get; set; } = new List<GenerateImage>();
 }
 
 public class GenerateImage
 {
     public long ProductVariantViewId { get; set; }
+    
+    public string PrintOrder { get; set; }
 }
