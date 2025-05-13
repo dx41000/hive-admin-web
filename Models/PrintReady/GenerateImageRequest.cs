2bbbdb50de2685e4f2b260577d3cf79afcc8430f
Michael McAiney <michael.mcainey@gmail.com>
2025-05-13 20:22:07 +0100

    Tidy Up

--- Models/PrintReady/GenerateImageRequest.cs
+++ Models/PrintReady/GenerateImageRequest.cs
@@ -3,14 +3,8 @@ namespace hive_admin_web.Models.PrintReady;
 
 public class GenerateImageRequest
 {
-    private static AppState _appState;
-    public GenerateImageRequest(AppState appState)
-    {
-        _appState = appState;
-    }
-
-    public long WorkspaceId = _appState.WorkspaceId;
-    public long StoreId = _appState.StoreId;
+    public long WorkspaceId { get; set; }
+    public long StoreId { get; set; }
     public long ProductVariantId { get; set; }
     public int ProductType { get; set; }
     public List<GenerateImage> GenerateImages { get; set; } = new List<GenerateImage>();
